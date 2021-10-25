8d77f7d6a0913099c4af6ec02f6f99e671c27472
Shohrat-Code <shohrat@code.edu.az>
2021-10-25 12:03:53 +0400

    Codes

---
+++ ControlStructure/ControlStructure/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace ControlStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sbyte number = sbyte.Parse(Console.ReadLine());
+
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("Musbet");
+            //}
+            //else if (number == 0)
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Menfi");
+            //}
+
+            //string season = Console.ReadLine();
+
+            //switch (season)
+            //{
+            //    case "yaz":
+            //        Console.WriteLine("Spring");
+            //        break;
+            //    case "yay":
+            //        Console.WriteLine("Summer");
+            //        break;
+            //    case "payiz":
+            //        Console.WriteLine("Autunm");
+            //        break;
+            //    case "qis":
+            //        Console.WriteLine("Winter");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You entered wrong season.");
+            //        break;
+            //}
+
+            //Console.WriteLine("Enter number a:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter number b:");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //if (a == b)
+            //{
+            //    Console.WriteLine(a + " and " + b + " are equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(a + " and " + b + " are not equal");
+            //}
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //if (a%2==0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    //if (i == 5)
+            //    //{
+            //    //    //continue;
+            //    //    break;   
+            //    //}
+
+            //    Console.WriteLine(i);
+            //}
+
+            //int i = 1;
+            //while (i == 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    Console.WriteLine("lkjdfv");
+            //    i++;
+            //} while (i==0);
+
+            //byte[] ages = { 15, 46, 79, 2, 34, 29 };
+
+            //foreach (byte age in ages)
+            //{
+            //    if (age>=18)
+            //    {
+            //        Console.WriteLine(age);
+            //    }
+            //}
+        }
+    }
+}
